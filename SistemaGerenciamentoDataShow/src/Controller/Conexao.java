a2d0731e839e53a4c1a1469edac0f09ae6929763
LucasMarcon11 <lucas11marcon@gmail.com>
2017-10-11 13:49:42 -0300

    Minha pasta do projeto, usem ela como referencia

---
+++ SistemaGerenciamentoDataShow/src/Controller/Conexao.java
@@ -0,0 +1,37 @@
+
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author LucasMarcon
+ *
+ * DESCRIÇÃO: oi sor
+ */
+
+public class Conexao {
+
+	public Connection AbrirConexao() {
+
+		Connection con = null;
+
+		try {
+
+			Class.forName("org.postgresql.Driver").newInstance();
+
+			String url		= "jdbc:postgresql://localhost:5432/dataShow";
+			String usuario	= "postgres";
+			String senha	= "lucas";
+
+			con = DriverManager.getConnection(url, usuario, senha);
+		} catch (SQLException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+			
+			System.out.println("Erro " + e.getMessage());
+		}
+
+		return con;
+	}
+}
