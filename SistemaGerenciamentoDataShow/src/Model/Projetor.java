14325374d480e91e7ed52cc73e6c6548e7a544d5
LucasMarcon11 <lucas11marcon@gmail.com>
2017-10-14 20:28:27 -0300

    FILHO DO TÍCHE

---
+++ SistemaGerenciamentoDataShow/src/Model/Projetor.java
@@ -0,0 +1,96 @@
+
+package Model;
+
+/**
+ *
+ * @author LucasMarcon
+ *
+ * DESCRIÇÃO: 
+ */
+
+public class Projetor {
+
+	private String idProjetor;
+	private String marca;
+	private String modelos;
+	private String numeroSerie;
+	private String dataCompra;
+	private String ansiLumens;
+	private String estado;
+	private String preco;
+	private String numeroHorasUso;
+
+	public String getIdProjetor() {
+		return idProjetor;
+	}
+
+	public void setIdProjetor(String idProjetor) {
+		this.idProjetor = idProjetor;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelos() {
+		return modelos;
+	}
+
+	public void setModelos(String modelos) {
+		this.modelos = modelos;
+	}
+
+	public String getNumeroSerie() {
+		return numeroSerie;
+	}
+
+	public void setNumeroSerie(String numeroSerie) {
+		this.numeroSerie = numeroSerie;
+	}
+
+	public String getDataCompra() {
+		return dataCompra;
+	}
+
+	public void setDataCompra(String dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+
+	public String getAnsiLumens() {
+		return ansiLumens;
+	}
+
+	public void setAnsiLumens(String ansiLumens) {
+		this.ansiLumens = ansiLumens;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getPreco() {
+		return preco;
+	}
+
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+	public String getNumeroHorasUso() {
+		return numeroHorasUso;
+	}
+
+	public void setNumeroHorasUso(String numeroHorasUso) {
+		this.numeroHorasUso = numeroHorasUso;
+	}
+	
+	
+}
